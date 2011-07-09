690850e5941eb24b24e3ac978ee24e3db7a0f89d
Quentin Pleplé <quentin.pleple@gmail.com>
2011-07-09 16:06:23 -0700

    fixed some ribbon spacing

--- home.php
+++ home.php
@@ -16,8 +16,7 @@
 	<?php while(have_posts()) : the_post(); ?>
 			<div id="content-title"><?php the_title(); ?></div>
 			<?php the_content(); ?>
-	<?php endwhile; ?>
-	ss
+	<?php endwhile; ?>	
 </div>
 <div id="page-bottom"></div>
 
