f3684127a425d1fa0de8202b88fe029407f6b6e4
Quentin Pleplé <quentin.pleple@gmail.com>
2011-07-11 15:27:40 -0700

    added footer

--- footer.php
+++ footer.php
@@ -1,4 +1,44 @@
     <div id="footer">
+        <div id="footer-left"></div>
+        <div id="footer-right"></div>
+        <div id="footer-content">
+            <div id="footer-columns">
+                <div class="footer-column">
+                    <div class="footer-column-title">Company</div>
+                    <ul>
+                        <li><a href="#">About Us</a></li>
+                        <li><a href="#">Blog</a></li>
+                        <li><a href="#">Twitter</a></li>
+                        <li><a href="#">Customers</a></li>
+                    </ul>
+                </div>
+                <div class="footer-column">
+                    <div class="footer-column-title">Products</div>
+                    <ul>
+                        <li><a href="#">Pricing</a></li>
+                        <li><a href="#">Features</a></li>
+                        <li><a href="#">Mission critical</a></li>
+                    </ul>
+                </div>
+                <div class="footer-column">
+                    <div class="footer-column-title">Legal</div>
+                    <ul>
+                        <li><a href="#">Terms of Service</a></li>
+                        <li><a href="#">Privacy Policy</a></li>
+                        <li><a href="#">Refund Policy</a></li>
+                    </ul>
+                </div>
+                <div class="footer-column">
+                    <div class="footer-column-title">Help</div>
+                    <ul>
+                        <li><a href="#">Wiki</a></li>
+                        <li><a href="#">Support</a></li>
+                        <li><a href="#">Contact Us</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div id="footer-bottom-note">© 2007-2011 Sclar, All rights reserved</div>
+        </div>
     </div>
     
     <?php wp_footer(); ?>
