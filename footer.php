27ae7e848e2977ae36467e5916682f645befd3cf
Quentin Pleplé <quentin.pleple@gmail.com>
2011-07-11 15:32:27 -0700

    moved footer content into web ui

--- footer.php
+++ footer.php
@@ -2,42 +2,13 @@
         <div id="footer-left"></div>
         <div id="footer-right"></div>
         <div id="footer-content">
-            <div id="footer-columns">
-                <div class="footer-column">
-                    <div class="footer-column-title">Company</div>
-                    <ul>
-                        <li><a href="#">About Us</a></li>
-                        <li><a href="#">Blog</a></li>
-                        <li><a href="#">Twitter</a></li>
-                        <li><a href="#">Customers</a></li>
-                    </ul>
-                </div>
-                <div class="footer-column">
-                    <div class="footer-column-title">Products</div>
-                    <ul>
-                        <li><a href="#">Pricing</a></li>
-                        <li><a href="#">Features</a></li>
-                        <li><a href="#">Mission critical</a></li>
-                    </ul>
-                </div>
-                <div class="footer-column">
-                    <div class="footer-column-title">Legal</div>
-                    <ul>
-                        <li><a href="#">Terms of Service</a></li>
-                        <li><a href="#">Privacy Policy</a></li>
-                        <li><a href="#">Refund Policy</a></li>
-                    </ul>
-                </div>
-                <div class="footer-column">
-                    <div class="footer-column-title">Help</div>
-                    <ul>
-                        <li><a href="#">Wiki</a></li>
-                        <li><a href="#">Support</a></li>
-                        <li><a href="#">Contact Us</a></li>
-                    </ul>
-                </div>
-            </div>
-            <div id="footer-bottom-note">© 2007-2011 Sclar, All rights reserved</div>
+            <?php $query = new WP_Query("name=footer"); ?>
+
+            <?php while ($query->have_posts()) : $query->the_post(); ?>
+            	<?php the_content(); ?>
+            <?php endwhile; ?>
+
+            <?php wp_reset_postdata(); // Reset Post Data ?>
         </div>
     </div>
     
